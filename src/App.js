b65ce3aa24abfacdee00810ad5f67ed4156a4ecc
Shantanu Sinha <shantanu.p.sinha@gmail.com>
2024-01-06 06:00:01 -0500

    final brokey

--- src/App.js
+++ src/App.js
@@ -25,26 +25,43 @@ import './App.css';
 
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Skills from './components/Skills';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <About />
+//       <Projects />
+//       <Skills />
+//       <Contact />
+//       <Footer />
+//       <h1 className="rainbow-text">Hi Rachael</h1>
+//     </div>
+//   );
+// }
+// 
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TestPage from './test';
+// import other components like HomePage if they exist
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer />
-      <h1 className="rainbow-text">Hi Rachael</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/test" element={<TestPage />} />
+        {/* Add other routes here if necessary */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TestPage = () => {
+  return <div>This is the test page!</div>;
+};
+
+export default TestPage;
